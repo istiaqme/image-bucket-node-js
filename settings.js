f50deda2c5f061931d381055b5975975ccbc4878
Istiaq Hasan <istiaq.me@gmail.com>
2021-11-04 12:30:16 +0600

    README Docs Updated

--- settings.js
+++ settings.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const BaseDirectory = __dirname;
-const StaticDir = path.join(BaseDirectory, '/static');
+const StaticDir = path.join(BaseDirectory, '/static'); // Static Directory In Which The Images Will Be Saved
 const SERVER_SETTINGS = {
-    APP_TITLE : 'Image Bucket',
-    APP_ROOT : 'http://localhost',
-    PORT : 5000,
-    WEB_URL : 'http://localhost:5000',
-    BUCKET_FOLDER_IN_URL : 'images'
+    APP_TITLE : 'Image Bucket', // Just A Name
+    APP_ROOT : 'http://localhost', // Express Server Root
+    PORT : 5000, // Express Server Port
+    WEB_URL : 'http://localhost:5000', // In Production Your Proxy URL eg. https://bucket.example.com
+    BUCKET_FOLDER_IN_URL : 'images' // Sub directory to be shown in url eg. https://bucket.example.com/images
 }
+
+
 
 
 module.exports = {
